3d5bc05447d46fc6f137e2997eb3dbf3f4df6fef
Kyle Pott <33555850+kylepott@users.noreply.github.com>
2024-08-09 23:36:33 -0400

    apos

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
             
               <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
                 {data.aboutpara}
-                <br /><br />What kind of work do we do <b>love</b> to do? What do we do <b>exceptionaly</b> well? We're glad you asked!<br />< br />
+                <br /><br />What kind of work do we do <b>love</b> to do? What do we do <b>exceptionaly</b> well? We&apos;re glad you asked!<br />< br />
              </p>
              
           </div>
