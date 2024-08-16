ef0a020e9246dee8e0a15e2e55db2c01def1b8de
Kyle Pott <33555850+kylepott@users.noreply.github.com>
2024-08-16 11:26:32 -0400

    meta

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en"> {/* Add your language code here */}
+    <Html lang="en">
       <Head>
-        {/* You can add additional meta tags here */}
+        <meta name="description" content="Decise helps clients get the most out of technology with tailored strategies, award-winning software engineering, data analytics, and cyber security services. We solve the toughest business challenges." />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Add any additional meta tags or links here */}
       </Head>
       <body>
         <Main />
